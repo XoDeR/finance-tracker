1db5bd4fc14bcac3a79a49d9a238d4da438ae479
XoDeR <xoder.vs@gmail.com>
2025-09-11 18:26:09 +0200

    login and register dto's

---
+++ FinanceTrackerApi/DTOs/RegisterDto.cs
@@ -0,0 +1,8 @@
+namespace FinanceTrackerApi.DTOs
+{
+    public class LoginDto
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+}
